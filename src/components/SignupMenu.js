4019e0d7ea0d350f6e459446695d33d54538b0a0
veselinmarkov <veselin.markov@gmail.com>
2021-07-19 13:15:01 +0200

    signup test change

--- src/components/SignupMenu.js
+++ src/components/SignupMenu.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Popover, InputBase, Button} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles((theme) => ({ 
     container: {
       display: "flex",
       gap: "10px",
